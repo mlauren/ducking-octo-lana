3a6cd04a63f5e92d85e1b877783a4126702a1038
Lauren M <mlmxwell@gmail.com>
2014-07-26 22:03:55 -0500

    Slowly becoming more drunk.

--- custom.js
+++ custom.js
@@ -1,27 +1,11 @@
 
-var loadButton = document.getElementById('load-more');
 
+/*
 var feed = new Instafeed({
         get: 'user',
         userId: 24516360,
         accessToken: '24516360.467ede5.740c179cb3174990be0a365b18f37bf9',
         clientId: '8f7c904f3eb8494e91e5cdb727c11a66',
-        target: 'target',
-        after: function() {
-          // disable button if no more results to load
-          if (!this.hasNext()) {
-            loadButton.setAttribute('disabled', 'disabled');
-          }
-        }
+        target: 'target'
     });
-
-if (!this.hasNext()) {
-      loadButton.setAttribute('disabled', 'disabled');
-    }
-
-document.addEventListener('load', function() {
-  loadButton.addEventListener('click', function() {
-    feed.next();
-  });
-});
-feed.run();
+*/
