b187f37f67c20e51890bed746ad608a0099694f4
Lauren M <mlmxwell@gmail.com>
2014-07-26 21:14:55 -0500

    Adds instagram feed stuff...

--- custom.js
+++ custom.js
@@ -1,9 +1,7 @@
 var feed = new Instafeed({
-        //get: 'user',
-        //userId: 24516360,
-        //accessToken: '24516360.467ede5.740c179cb3174990be0a365b18f37bf9',
-        get: 'tagged',
-        tagName: 'awesome',
+        get: 'user',
+        userId: 24516360,
+        accessToken: '24516360.467ede5.740c179cb3174990be0a365b18f37bf9',
         clientId: '8f7c904f3eb8494e91e5cdb727c11a66',
         target: 'target'
     });
